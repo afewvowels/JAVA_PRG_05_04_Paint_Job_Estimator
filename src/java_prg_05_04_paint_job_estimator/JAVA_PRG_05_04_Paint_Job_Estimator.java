fa6e7d5f02d5f6fd2cb65427164d7875cdaabf4a
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-12 13:44:32 -0500

    Prompt written.

---
+++ src/java_prg_05_04_paint_job_estimator/JAVA_PRG_05_04_Paint_Job_Estimator.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_05_04_paint_job_estimator;
+
+/**
+ *
+ * @author bluebackdev
+ * A painting company has determined that for every 115 square feet of wall
+ * space, one gallon of paint land eight hours of labor will be required. The
+ * company charges $18.00 per hour for labor. Write a program that allows the
+ * user to enter the number of rooms to be painted and the price of the paint
+ * per gallon. It should also ask for the square feet of wall space in each
+ * room. The program should have methods that return the following data:
+ * 
+ *      * The number of gallons of paint required
+ *      * The hours of labor required
+ *      * The cost of the paint
+ *      * The labor charges
+ *      * The total cost of the paint job
+ * 
+ * Then it should display the data on the screen.
+ */
+public class JAVA_PRG_05_04_Paint_Job_Estimator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
